9b41e9c8522bb5a07961b8faad37433562e2b780
Feleky Mirkó <feleky@gmail.com>
2018-08-23 16:22:18 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const schema = require('./schema/schema');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const settings = require('./settings');
+
+const server = express();
+server.use(cors());
+
+mongoose.connect("mongodb://Lunatial:test123@ds125402.mlab.com:25402/gqlbookdb");
+mongoose.connection.once('open', () => console.log('connected to database'));
+
+server.use('/graphql', graphqlHTTP({
+    schema: schema,
+    graphiql: true
+}));
+
+server.listen(settings.port, (err) => {
+    if (err) throw err;
+    console.log(`> Ready on http://localhost:${settings.port}`)
+});
